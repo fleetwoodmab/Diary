dc7225db10f6b5f67bb7b86ab370f5e5d6c5ae9e
mab <73135677+hacoeur@users.noreply.github.com>
2023-04-25 10:00:10 +0100

    added image function

--- app/src/main/java/ie/setu/diary/models/DiaryModel.kt
+++ app/src/main/java/ie/setu/diary/models/DiaryModel.kt
@@ -1,9 +1,10 @@
 package ie.setu.diary.models
 
+import android.net.Uri
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import java.util.Date
 
 @Parcelize
-data class DiaryModel(var id: Long = 0, var title: String = "", var description: String = "", var date: Date=Date(2023,1, 1)) : Parcelable {
+data class DiaryModel(var id: Long = 0, var title: String = "", var description: String = "", var date: Date=Date(2023,1, 1), var image: Uri = Uri.EMPTY) : Parcelable {
 }
